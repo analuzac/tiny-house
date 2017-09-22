bed221b1ada567c51e12bf461c6cc1ba7355d60c
Ana Luz Acevedo-Cabrera <analuzacevedo-cabrera@Anas-MacBook.local>
2017-09-22 11:04:03 -0700

    hardcoded sort & filter working

--- src/api/getListings.js
+++ src/api/getListings.js
@@ -1,13 +1,27 @@
-export default function getListings() {
-  return fetch('https://api.airtable.com/v0/appHq8T1Eu4rcibs9/TinyHouse?', {
-    method: 'GET',
-    headers: {
-      Authorization: 'Bearer keyE9lXfaaEAGEG23',
-      'Content-Type': 'application/json'
+//sort=[{field: "rent", direction: "desc"}]
+
+export default function getListings(/* { filter } */) {
+  let filter = 'Berkeley, CA';
+  filter = filter
+    ? '&filterByFormula=' + encodeURI(`({coordinates} = '${filter}')`)
+    : '';
+  // this didn't work --> const sortBy = encodeURI('[{field: "rent", direction: "desc"}]');
+  let sort = 'sort[0][field]=rent';
+
+  return fetch(
+    `https://api.airtable.com/v0/appHq8T1Eu4rcibs9/TinyHouse?${sort}${filter}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer keyE9lXfaaEAGEG23',
+        'Content-Type': 'application/json'
+      }
     }
-  })
+  )
     .then(response => response.json())
     .then(data => {
+      console.log('ma data', data);
+      // debugger;
       return data.records.map(record => {
         return {
           id: record.id,
