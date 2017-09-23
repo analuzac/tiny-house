60a2e6b619112f7f7fc63fd1502068a1f3129546
Ana Luz Acevedo-Cabrera <analuzacevedo-cabrera@Anas-MacBook.local>
2017-09-22 21:01:44 -0700

    including more tests

---
+++ src/components/EditableWidgetComponent.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import EditableWidgetComponent from './EditableWidgetComponent';
+
+describe('EditableWidgetComponent', () => {
+  let hostInfo = {
+    rent: 500,
+    dimensions: 300,
+    coordinates: 'Berkeley, CA',
+    date: 'September 23, 2017',
+    amenities: 'washer/dryer, water hose, compost/recycling/trash bins'
+    //   imagePath: '//via.placeholder.com/300x200'
+  };
+
+  it('Renders EditableWidgetComponent', () => {
+    const wrapper = shallow(
+      <EditableWidgetComponent hostInfo={hostInfo} onDelete={onDelete} />
+    );
+    expect(wrapper.find('h4').text()).toBe(hostInfo.coordinates);
+  });
+
+  it('Triggers onDelete', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(
+      <EditableWidgetComponent hostInfo={hostInfo} onDelete={onDelete} />
+    );
+    wrapper.find('.btn-large').simulate('click', { preventDefault: () => {} });
+    expect(onDelete).toBeCalled();
+  });
+});
---
+++ src/components/WidgetComponent.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import WidgetComponent from './WidgetComponent';
+
+describe('WidgetComponent', () => {
+  let hostInfo = {
+    rent: 500,
+    dimensions: 300,
+    coordinates: 'Berkeley, CA',
+    date: 'September 23, 2017',
+    amenities: 'washer/dryer, water hose, compost/recycling/trash bins'
+    //   imagePath: '//via.placeholder.com/300x200'
+  };
+
+  it('Renders WidgetComponent', () => {
+    const wrapper = shallow(
+      <WidgetComponent hostInfo={hostInfo} onLove={onLove} />
+    );
+    expect(wrapper.find('h4').text()).toBe(hostInfo.coordinates);
+  });
+
+  it('Triggers onLove', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(
+      <WidgetComponent hostInfo={hostInfo} onLove={onLove} />
+    );
+    wrapper
+      .find('.btn-floating')
+      .simulate('click', { preventDefault: () => {} });
+    expect(onLove).toBeCalled();
+  });
+});
