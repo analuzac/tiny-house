5e1c6f9ad3045743e8f284a5d3d33cee7a782e3f
Ana Luz Acevedo-Cabrera <analuzacevedo-cabrera@Anas-MacBook.local>
2017-11-15 12:44:32 -0800

    added validation to SignUp and LogIn forms

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -1,56 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function LogInComponent({ userInfo, onLogIn }) {
+export default class LogInComponent extends Component {
+  //{ userInfo, onLogIn, history }) {
   //console.log('LOGIN_PROPS', props);
-  function handleSubmit(event) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showBody: true,
+      hasValidationErrors: false
+    };
+  }
+
+  handleSubmit = event => {
     event.preventDefault();
     const $form = event.target;
     const userInfo = {};
     userInfo.email = $form.email.value;
     userInfo.password = $form.password.value;
     console.log('USER LOGGED IN', userInfo);
-    onLogIn(userInfo);
+
+    let regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (userInfo.email.match(regEmail) && userInfo.password.length >= 8) {
+      this.props.onLogIn(userInfo);
+    } else this.setState({ hasValidationErrors: true });
+  };
+
+  render() {
+    return (
+      <form id="form" className="col s12 m12" onSubmit={this.handleSubmit}>
+        <div id="title" className="col s12 m12">
+          <h4 className="green-text">Log In</h4>
+        </div>
+        <div className="row">
+          <div className="input-field col s12 m12">
+            <i className="material-icons prefix green-text">email</i>
+            <input
+              id="email"
+              type="text"
+              className="validate"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s12 m12">
+            <i className="material-icons prefix green-text">lock</i>
+            <input
+              id="password"
+              type="password"
+              className="validate"
+              placeholder="Password"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="row center">
+          <button
+            id="submit-button"
+            type="submit"
+            name="action"
+            className="btn-large waves-effect waves-light green lighten-1">
+            LOG IN
+          </button>
+          {this.state.hasValidationErrors &&
+            <h5 style={styles.errorStyle}>Please submit valid inputs</h5>}
+        </div>
+      </form>
+    );
   }
-  return (
-    <form id="form" className="col s12 m12" onSubmit={handleSubmit}>
-      <div id="title" className="col s12 m12">
-        <h4 className="green-text">Log In</h4>
-      </div>
-      <div className="row">
-        <div className="input-field col s12 m12">
-          <i className="material-icons prefix green-text">email</i>
-          <input
-            id="email"
-            type="text"
-            className="validate"
-            placeholder="Email Address"
-            required
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field col s12 m12">
-          <i className="material-icons prefix green-text">lock</i>
-          <input
-            id="password"
-            type="password"
-            className="validate"
-            placeholder="Password"
-            required
-          />
-        </div>
-      </div>
-
-      <div className="row center">
-        <button
-          id="submit-button"
-          type="submit"
-          name="action"
-          className="btn-large waves-effect waves-light green lighten-1">
-          LOG IN
-        </button>
-      </div>
-    </form>
-  );
 }
+
+const styles = {
+  errorStyle: {
+    color: 'red',
+    fontWeight: 'bold'
+  }
+};
--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //import Form from 'react-router-form';
 // For when "Form" starts working as expected:
 // <Form to={'/login'} method="GET" ...
 
-export default function SignUpComponent({ onSignUp, newUser, history }) {
-  function handleSubmit(event) {
+export default class SignUpComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showBody: true,
+      hasValidationErrors: false
+    };
+  }
+
+  handleSubmit = event => {
     event.preventDefault();
     const $form = event.target;
     const newUser = {};
@@ -13,64 +21,81 @@ export default function SignUpComponent({ onSignUp, newUser, history }) {
     newUser.email = $form.email.value;
     newUser.password = $form.password.value;
     console.log('USER SIGNED UP', newUser);
-    onSignUp(newUser);
-    history.push('/login');
+
+    let regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (newUser.email.match(regEmail) && newUser.password.length >= 8) {
+      this.props.onSignUp(newUser);
+      this.props.history.push('/login');
+    } else this.setState({ hasValidationErrors: true });
+  };
+
+  render() {
+    //console.log(this.state, 'these are the props');
+    return (
+      <form id="form" className="col s12 m12" onSubmit={this.handleSubmit}>
+        <div id="title" className="col s12 m12">
+          <h4 className="green-text">Sign Up</h4>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s12 m12">
+            <i className="material-icons prefix green-text">person</i>
+            <input
+              id="name"
+              type="text"
+              className="validate"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s12 m12">
+            <i className="material-icons prefix green-text">email</i>
+            <input
+              id="email"
+              type="text"
+              className="validate"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s12 m12">
+            <i className="material-icons prefix green-text">lock</i>
+            <input
+              id="password"
+              type="password"
+              className="validate"
+              placeholder="Create a Password"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="row center">
+          <button
+            id="submit-button"
+            type="submit"
+            name="action"
+            className="btn-large waves-effect waves-light green lighten-1">
+            SIGN UP
+          </button>
+          {this.state.hasValidationErrors &&
+            <h5 style={styles.errorStyle}>Please submit valid inputs</h5>}
+        </div>
+      </form>
+    );
   }
-
-  return (
-    <form id="form" className="col s12 m12" onSubmit={handleSubmit}>
-      <div id="title" className="col s12 m12">
-        <h4 className="green-text">Sign Up</h4>
-      </div>
-
-      <div className="row">
-        <div className="input-field col s12 m12">
-          <i className="material-icons prefix green-text">person</i>
-          <input
-            id="name"
-            type="text"
-            className="validate"
-            placeholder="Full Name"
-            required
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field col s12 m12">
-          <i className="material-icons prefix green-text">email</i>
-          <input
-            id="email"
-            type="text"
-            className="validate"
-            placeholder="Email Address"
-            required
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="input-field col s12 m12">
-          <i className="material-icons prefix green-text">lock</i>
-          <input
-            id="password"
-            type="password"
-            className="validate"
-            placeholder="Create a Password"
-            required
-          />
-        </div>
-      </div>
-
-      <div className="row center">
-        <button
-          id="submit-button"
-          type="submit"
-          name="action"
-          className="btn-large waves-effect waves-light green lighten-1">
-          SIGN UP
-        </button>
-      </div>
-    </form>
-  );
 }
+
+const styles = {
+  errorStyle: {
+    color: 'red',
+    fontWeight: 'bold'
+  }
+};
